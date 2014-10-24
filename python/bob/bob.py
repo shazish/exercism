1374b8469a353b825cb8522f99f4f7928e4f8e96
Shaahin Shahbazi <shaahin@gmail.com>
2014-10-24 14:44:29 -0500

    Python bob exercise done.

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -2,5 +2,12 @@
 # Skeleton file for the Python "Bob" exercise.
 #
 def hey(what):
-
-    return
+	if what == what.upper() and what != what.lower():
+		return "Whoa, chill out!"
+	elif what.strip().endswith("?"):
+		return "Sure."		
+	elif what.strip() == "":
+		return "Fine. Be that way!"
+	else:
+		return "Whatever."
+			
