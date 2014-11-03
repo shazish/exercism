00862b136a1ede38ae682b5cd789c14320fdedfb
Shaahin Shahbazi <shaahin@gmail.com>
2014-11-03 12:15:05 -0600

    JS beer song done

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -4,6 +4,8 @@ var verse = function(input) {
 			return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
 		case 1:
 			return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
+		case 2:
+			return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n";			
 		default:
 			return input.toString() + " bottles of beer on the wall, " + input.toString() + " bottles of beer.\nTake one down and pass it around, " + (input - 1).toString() + " bottles of beer on the wall.\n";
 	}
