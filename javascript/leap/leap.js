3ffff448d84c10e13ba0936294250dc556927149
Shaahin Shahbazi <shaahin@gmail.com>
2014-11-05 13:54:06 -0600

    JS leap done

---
+++ javascript/leap/leap.js
@@ -0,0 +1,14 @@
+function leap(year) {
+	if (year % 4 == 0)
+		if (year % 100 == 0)
+			if (year % 400 == 0)
+				return true
+			else 
+				return false
+		else
+			return true
+	else
+		return false
+};
+
+module.exports = leap;
